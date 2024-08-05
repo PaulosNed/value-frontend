9c9391d299d8434662602a478cb268de14e67195
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2024-08-05 16:37:14 +0300

    made nav bar responsive

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,15 +1,10 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState } from "react";
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import { Button } from "../ui/button";
@@ -34,6 +29,9 @@ const NavBar = () => {
       link: "/community",
     },
   ];
+
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+
   return (
     <NavigationMenu>
       <div className="w-screen px-6 py-4 md:px-10 md:py-6 flex justify-between items-center z-20">
@@ -73,7 +71,48 @@ const NavBar = () => {
             <Link href={"/signup"}>Sign up</Link>
           </Button>
         </div>
-        
+
+        {/* mobile burger */}
+        {/* Mobile view */}
+        <div className="block md:hidden">
+          <div className="flex flex-col items-end relative">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <img src={"/images/burger.svg"} alt="" width={40} height={40} />
+            </button>
+
+            {/* Navigation section */}
+            {isMenuToggled && (
+              <div className="flex flex-col w-44 py-5 pl-8 space-y-2 absolute top-10 right-0 bg-secondary shadow-lg">
+                {navItems.map((nav, idx) => (
+                  <div key={nav.name} onClick={() => setIsMenuToggled(false)}>
+                    <Link href={nav.link} className="text-primary">
+                      {nav.name}
+                    </Link>
+                    {/* <NavLink name={nav.name} link={nav.link} /> */}
+                  </div>
+                ))}
+                <div className="h-1"></div>
+                <hr className="-ml-6 mr-2" />
+                <div className="h-1"></div>
+                <Link href={"/login"} className="text-primary">
+                  Login
+                </Link>
+                <Link href={"/signup"} className="text-primary">
+                  Signup
+                </Link>
+                {/* <Button
+                  asChild
+                  className="-ml-5 mx-3 px-3 border border-primary"
+                  variant="outline"
+                >
+                  <Link href={"/login"}>Log in</Link>
+                </Button> */}
+                {/* <button className="btn">Login</button>
+              <button className="btn">Donate</button> */}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </NavigationMenu>
   );
